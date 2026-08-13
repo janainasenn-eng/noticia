1f800389393de263d42e966452bd07f20ec97b6c
janainasenn-eng <janaina.senn@escola.pr.gov.br>
2026-08-13 17:25:21 -0300

    ha

--- script.js
+++ script.js
@@ -1,10 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
-    // Garantindo a consistência da exibição de metadados
-    const dataElemento = document.getElementById("data-publicacao");
-    
-    // Verificação de carregamento da imagem para diagnóstico no console
-    const imgElemento = document.getElementById("imagem-noticia");
-    imgElemento.onerror = function () {
-        console.warn("Atenção: A imagem 'images.jpeg' precisa estar na mesma pasta dos arquivos HTML/CSS/JS.");
-    };
+
+    const botao = document.getElementById("aumentarTexto");
+
+
+    botao.addEventListener("click", function () {
+
+        document.body.classList.toggle("texto-grande");
+
+
+        if (document.body.classList.contains("texto-grande")) {
+
+            botao.textContent = "A−";
+
+            botao.setAttribute(
+                "aria-label",
+                "Diminuir tamanho do texto"
+            );
+
+        } else {
+
+            botao.textContent = "A+";
+
+            botao.setAttribute(
+                "aria-label",
+                "Aumentar tamanho do texto"
+            );
+
+        }
+
+    });
+
 });
